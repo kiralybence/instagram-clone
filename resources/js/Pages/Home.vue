06ab7217c28d68438c23c08a38f1e440bc09c29f
Bence Király <kiralybencepecs@gmail.com>
2022-04-09 21:46:02 +0200

    Added posts

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -46,5 +46,44 @@ import AppLayout from '@/Layouts/AppLayout.vue';
         </div>
 
         <hr style="border-top: 1px solid #474747;">
+
+        <!-- Post -->
+        <div>
+            <!-- Uploader -->
+            <div class="flex align-middle justify-between p-3">
+                <div class="flex">
+                    <img src="https://via.placeholder.com/150x150" class="rounded-full" style="width: 30px; height: 30px;">
+                    <span class="ml-3">username</span>
+                </div>
+
+                <div class="flex">
+                    ...
+                </div>
+            </div>
+
+            <!-- Image -->
+            <div>
+                <img src="https://via.placeholder.com/800x800">
+            </div>
+
+            <!-- Actions -->
+            <div class="flex align-middle justify-between p-3" style="font-size: 1.5rem;">
+                <div class="flex">
+                    <!-- Like -->
+                    <i class="fa-regular fa-heart mr-3"></i>
+
+                    <!-- Comment -->
+                    <i class="fa-regular fa-comment mr-3"></i>
+
+                    <!-- Send -->
+                    <i class="fa-regular fa-paper-plane mr-3"></i>
+                </div>
+
+                <div class="flex">
+                    <!-- Save -->
+                    <i class="fa-regular fa-bookmark ml-3"></i>
+                </div>
+            </div>
+        </div>
     </AppLayout>
 </template>
