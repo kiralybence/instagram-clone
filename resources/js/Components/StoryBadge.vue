62d931e6382c859d36c6e15ec351b54a0f3df632
Bence Király <kiralybencepecs@gmail.com>
2022-04-23 18:09:40 +0200

    Separated stories to a custom component

---
+++ resources/js/Components/StoryBadge.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="text-center mx-2">
+        <!-- Icon -->
+        <div
+            class="rounded-full"
+            style="
+                width: 60px;
+                height: 60px;
+                border: 2px solid #474747;
+                padding: 3px;
+            "
+        >
+            <img src="https://via.placeholder.com/150x150" class="rounded-full mb-1 w-full h-full">
+        </div>
+
+        <!-- Username -->
+        <span style="font-size: 0.6rem;">TODO</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "StoryBadge",
+    props: {
+        // story: Object,
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,49 +1,16 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
 import Post from "../Components/Post";
+import StoryBadge from "../Components/StoryBadge";
 </script>
 
 <template>
     <AppLayout title="Home">
         <!-- Stories -->
         <div class="flex mb-3">
-            <!-- Container -->
-            <div class="text-center mx-2">
-                <!-- Icon -->
-                <div
-                    class="rounded-full"
-                    style="
-                        width: 60px;
-                        height: 60px;
-                        border: 2px solid #474747;
-                        padding: 3px;
-                    "
-                >
-                    <img src="https://via.placeholder.com/150x150" class="rounded-full mb-1 w-full h-full">
-                </div>
-
-                <!-- Username -->
-                <span style="font-size: 0.6rem;">Your story</span>
-            </div>
-
-            <!-- Container -->
-            <div class="text-center mx-2">
-                <!-- Icon -->
-                <div
-                    class="rounded-full"
-                    style="
-                        width: 60px;
-                        height: 60px;
-                        border: 2px solid #474747;
-                        padding: 3px;
-                    "
-                >
-                    <img src="https://via.placeholder.com/150x150" class="rounded-full mb-1 w-full h-full">
-                </div>
-
-                <!-- Username -->
-                <span style="font-size: 0.6rem;">TODO</span>
-            </div>
+            <StoryBadge />
+            <StoryBadge />
+            <StoryBadge />
         </div>
 
         <hr style="border-top: 1px solid #474747;">
