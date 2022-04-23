1252663d96bf81bd1aa078738c6fff76ffe15b63
Bence Király <kiralybencepecs@gmail.com>
2022-04-23 18:06:48 +0200

    Separated posts to a custom component

---
+++ resources/js/Components/Post.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="mb-3">
+        <!-- Uploader -->
+        <div class="flex align-middle justify-between p-3">
+            <!-- Left -->
+            <div class="flex">
+                <img :src="post.user.profile_photo_url" class="rounded-full" style="width: 30px; height: 30px;">
+                <span class="ml-3">{{ post.user.name }}</span>
+            </div>
+
+            <!-- Right -->
+            <div class="flex">
+                ...
+            </div>
+        </div>
+
+        <!-- Image -->
+        <div class="w-full">
+            <img :src="post.image.url" class="w-full">
+        </div>
+
+        <!-- Below image -->
+        <div class="p-3">
+            <!-- Actions -->
+            <div class="flex align-middle justify-between" style="font-size: 1.5rem;">
+                <!-- Left -->
+                <div class="flex">
+                    <!-- Like -->
+                    <i class="fa-regular fa-heart mr-3"></i>
+
+                    <!-- Comment -->
+                    <i class="fa-regular fa-comment mr-3"></i>
+
+                    <!-- Send -->
+                    <i class="fa-regular fa-paper-plane mr-3"></i>
+                </div>
+
+                <!-- Right -->
+                <div class="flex">
+                    <!-- Save -->
+                    <i class="fa-regular fa-bookmark ml-3"></i>
+                </div>
+            </div>
+
+            <!-- Info -->
+            <div class="mt-3">
+                <b>1234 likes</b><br>
+                <b>{{ post.user.name }}</b> {{ post.description }}<br>
+                <span style="color: darkgray;">{{ post.created_at_ago }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Post",
+    props: {
+        post: Object,
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
+import Post from "../Components/Post";
 </script>
 
 <template>
@@ -47,58 +48,8 @@ import AppLayout from '@/Layouts/AppLayout.vue';
 
         <hr style="border-top: 1px solid #474747;">
 
-        <!-- Post -->
-        <div v-for="post in posts" class="mb-3">
-            <!-- Uploader -->
-            <div class="flex align-middle justify-between p-3">
-                <!-- Left -->
-                <div class="flex">
-                    <img :src="post.user.profile_photo_url" class="rounded-full" style="width: 30px; height: 30px;">
-                    <span class="ml-3">{{ post.user.name }}</span>
-                </div>
-
-                <!-- Right -->
-                <div class="flex">
-                    ...
-                </div>
-            </div>
-
-            <!-- Image -->
-            <div class="w-full">
-                <img :src="post.image.url" class="w-full">
-            </div>
-
-            <!-- Below image -->
-            <div class="p-3">
-                <!-- Actions -->
-                <div class="flex align-middle justify-between" style="font-size: 1.5rem;">
-                    <!-- Left -->
-                    <div class="flex">
-                        <!-- Like -->
-                        <i class="fa-regular fa-heart mr-3"></i>
-
-                        <!-- Comment -->
-                        <i class="fa-regular fa-comment mr-3"></i>
-
-                        <!-- Send -->
-                        <i class="fa-regular fa-paper-plane mr-3"></i>
-                    </div>
-
-                    <!-- Right -->
-                    <div class="flex">
-                        <!-- Save -->
-                        <i class="fa-regular fa-bookmark ml-3"></i>
-                    </div>
-                </div>
-
-                <!-- Info -->
-                <div class="mt-3">
-                    <b>1234 likes</b><br>
-                    <b>{{ post.user.name }}</b> {{ post.description }}<br>
-                    <span style="color: darkgray;">{{ post.created_at_ago }}</span>
-                </div>
-            </div>
-        </div>
+        <!-- Posts -->
+        <Post v-for="post in posts" :post="post" />
     </AppLayout>
 </template>
 
