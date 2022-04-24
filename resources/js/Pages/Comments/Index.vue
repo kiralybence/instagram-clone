68d02681d61a6e5192f4d087bc52b6c1db7266c5
Bence Király <kiralybencepecs@gmail.com>
2022-04-24 19:04:13 +0200

    Added post description to comments page

--- resources/js/Pages/Comments/Index.vue
+++ resources/js/Pages/Comments/Index.vue
@@ -5,6 +5,25 @@ import Comment from "../../Components/Comment";
 
 <template>
     <AppLayout title="Comments" :display-navbar="false">
+        <!-- Post description -->
+        <div class="flex p-3">
+            <!-- Left -->
+            <img :src="post.user.profile_photo_url" class="rounded-full" style="width: 30px; height: 30px;">
+
+            <!-- Center -->
+            <div class="pl-3 w-full">
+                <b>{{ post.user.name }}</b>
+
+                {{ post.description }}
+
+                <div style="color: darkgray;">
+                    <span class="mr-5">
+                        {{ post.created_at_ago }}
+                    </span>
+                </div>
+            </div>
+        </div>
+
         <hr style="border-top: 1px solid #474747;">
 
         <!-- Comments -->
