60b55211ecc2d7ab4393eecc687cc05d81aca4b6
Bence Király <kiralybencepecs@gmail.com>
2022-04-24 18:09:10 +0200

    Added auto-hide to replies

--- resources/js/Components/Comment.vue
+++ resources/js/Components/Comment.vue
@@ -1,63 +1,85 @@
 <script setup>
-import { Link } from '@inertiajs/inertia-vue3'
+import { Link } from '@inertiajs/inertia-vue3';
 </script>
 
 <template>
     <div
-        class="flex p-3"
         :class="{
-            'ml-10': comment.is_reply,
+            'mb-3': !comment.is_reply,
         }"
     >
-        <!-- Left -->
-        <img :src="comment.user.profile_photo_url" class="rounded-full" style="width: 30px; height: 30px;">
+        <div
+            class="flex p-3"
+            :class="{
+                'ml-10': comment.is_reply,
+            }"
+        >
+            <!-- Left -->
+            <img :src="comment.user.profile_photo_url" class="rounded-full" style="width: 30px; height: 30px;">
 
-        <!-- Center -->
-        <div class="pl-3 w-full">
-            <b>{{ comment.user.name }}</b>
+            <!-- Center -->
+            <div class="pl-3 w-full">
+                <b>{{ comment.user.name }}</b>
 
-            <span
-                class="text-blue-100"
-                v-if="comment.is_reply"
-            >
-                <Link href="#">
-                    @{{ comment.parent.user.name }}
-                </Link>
-            </span>
-
-            {{ comment.content }}
-
-            <div style="color: darkgray;">
-                <span class="mr-5">
-                    {{ comment.created_at_ago }}
+                <span
+                    class="text-blue-100"
+                    v-if="comment.is_reply"
+                >
+                    <Link href="#">
+                        @{{ comment.parent.user.name }}
+                    </Link>
                 </span>
 
-                <span class="mr-5">
-                    {{ comment.like_count }} {{ comment.like_count !== 1 ? 'likes' : 'like' }}
-                </span>
+                {{ comment.content }}
 
-                <span class="mr-5">
-                    Reply
-                </span>
+                <div style="color: darkgray;">
+                    <span class="mr-5">
+                        {{ comment.created_at_ago }}
+                    </span>
+
+                    <span class="mr-5">
+                        {{ comment.like_count }} {{ comment.like_count !== 1 ? 'likes' : 'like' }}
+                    </span>
+
+                    <span class="mr-5">
+                        Reply
+                    </span>
+                </div>
+            </div>
+
+            <!-- Right -->
+            <div class="align-middle ml-3">
+                <!-- Like -->
+                <i
+                    class="fa-heart cursor-pointer"
+                    :class="{
+                        'fa-regular': !comment.is_liked,
+                        'fa-solid': comment.is_liked,
+                    }"
+                    @click="toggleLikeStatus"
+                ></i>
             </div>
         </div>
 
-        <!-- Right -->
-        <div class="align-middle">
-            <!-- Like -->
-            <i
-                class="fa-heart cursor-pointer"
-                :class="{
-                    'fa-regular': !comment.is_liked,
-                    'fa-solid': comment.is_liked,
-                }"
-                @click="toggleLikeStatus"
-            ></i>
+        <div
+            class="ml-14"
+            v-if="!showReplies && !comment.is_reply && comment.replies.length > 0"
+        >
+            <span
+                class="cursor-pointer"
+                @click="showReplies = true"
+            >
+                View {{ comment.replies.length }} more {{ comment.replies.length !== 1 ? 'replies' : 'reply' }}
+            </span>
         </div>
-    </div>
 
-    <!-- Replies -->
-    <Comment v-for="comment in comment.replies" :comment="comment" />
+        <!-- Replies -->
+        <Comment
+            v-for="comment in comment.replies"
+            :comment="comment"
+            v-if="showReplies"
+        />
+    </div>
 </template>
 
 <script>
@@ -65,6 +87,11 @@ export default {
     name: "Comment",
     props: {
         comment: Object,
+    },
+    data() {
+        return {
+            showReplies: false,
+        };
     },
     methods: {
         async toggleLikeStatus() {
