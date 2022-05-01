0520b01c66f84a73d6bee035b1523bbaba9cea82
Bence Király <kiralybencepecs@gmail.com>
2022-05-01 21:01:43 +0200

    Added TODO

--- resources/js/Components/FooterMenu.vue
+++ resources/js/Components/FooterMenu.vue
@@ -4,6 +4,8 @@ import FooterMenuIcon from './FooterMenuIcon';
 
 <template>
     <div class="flex justify-between items-center fixed bottom-0 w-full bg-black text-white py-2" style="font-size: 1.3rem; border-top: 1px solid #474747;">
+        <!-- TODO: icons should be solid when active, regular when not active -->
+
         <FooterMenuIcon :href="route('home')">
             <i class="fa-solid fa-house"></i>
         </FooterMenuIcon>
